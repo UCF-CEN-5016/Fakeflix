72ff09f091fb87566211c4e51e9ab2c01e88751f
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2024-09-11 18:58:36 -0400

    Added docs to `src/App.js`

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// Provides the entrypoint for this React app to be rendered in `./index.js`.
+
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch, Redirect, useLocation } from "react-router-dom";
@@ -18,6 +20,19 @@ import { selectCurrentUser } from './redux/auth/auth.selectors';
 import { selectSearchResults } from "./redux/search/search.selectors";
 import { checkUserSession } from "./redux/auth/auth.actions";
 
+/**
+ * React App Entrypoint
+ * 
+ * This app uses client side rendering (CSR) and specifically, is a single page application (SPA),
+ * meaning that the entire app is loaded all at once.
+ * 
+ * This component provides a common layout and the CSR router.
+ * 
+ * If the user is logged in, a navigation bar and movie details menu will be rendered.
+ * The router is animated to fade out and in when navigating.
+ * 
+ * @returns {JSX.Element}
+ */
 const App = () => {
 
     const currentUser = useSelector(selectCurrentUser);
