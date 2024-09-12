00a179db7347bb2731428684ac0a2ee9d8bac9fb
HashDBrown <halkhateeb704@gmail.com>
2024-09-12 11:32:18 -0400

    Added in-line comments to useRetrieveData.js for a better understanding of the file.

--- src/hooks/useRetrieveData.js
+++ src/hooks/useRetrieveData.js
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux" //Redux is a state management for javascript applications. It provides a global store where you keep the application state
 import { fetchMovieDataConfig, fetchPopularDataConfig, fetchSeriesDataConfig } from "../dataConfig";
 
-export const useRetrieveData = (type) => {
+export const useRetrieveData = (type) => { //function to retrieve data. It takes a type as an argument. type should be either movies, series, or popular
 
-    const dispatch = useDispatch()
-    const [data, setData] = useState(null)
+    const dispatch = useDispatch() //allows for access of the dispatch function from the Redux store
+    const [data, setData] = useState(null) //initalizes the data state to null. setData function will be used to update data state
 
-    useEffect(() => {
-        let selectedConfigArray = null;
+    useEffect(() => { 
+        let selectedConfigArray = null; //this variable will be used to store the configuration array based on the type
         switch (type) {
-            case "movies":
+            case "movies": //if the type is movies, then the selectedConfigArray will be fetchMovieDataConfig.
                 selectedConfigArray = fetchMovieDataConfig;
                 break;
-            case "series":
+            case "series": //if the type is series, then the selectedConfigArray will be fetchSeriesDataConfig
                 selectedConfigArray = fetchSeriesDataConfig;
                 break;
-            case "popular":
+            case "popular": //if the type is popular, then the selectedConfigArray will be fetchPopularDataConfig
                 selectedConfigArray = fetchPopularDataConfig;
                 break;
             default:
@@ -24,9 +24,9 @@ export const useRetrieveData = (type) => {
         }
 
         let isPage = true;
-        const rowsData = selectedConfigArray.map(el => {
-            dispatch(el.thunk(el.url, isPage))
-            return {
+        const rowsData = selectedConfigArray.map(el => { //loops through the selectedConfigArray and returns a new array that is stored in rowsData
+            dispatch(el.thunk(el.url, isPage)) //dispatches the thunk function with the url and boolean variabe isPage as arguments
+            return { //returns an object with these properties
                 id: el.id,
                 title: el.title,
                 genre: el.genre,
@@ -34,9 +34,9 @@ export const useRetrieveData = (type) => {
                 isLarge: el.isLarge
             }
         })
-        setData(rowsData)
+        setData(rowsData) //sets the data state to the rowsData array
 
-    }, [type, dispatch])
+    }, [type, dispatch]) //useEffect runs when the type or dispatch changes
 
     return data
 }
