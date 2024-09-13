86b836ba5c5e5cf7193e3f984bf5633260a4f078
Miguel Rosario <miguelangel53013@gmail.com>
2024-09-13 19:51:17 -0400

    Consolidated comments into a better formatting for each section of the code.

--- src/components/DetailModal/DetailModal.jsx
+++ src/components/DetailModal/DetailModal.jsx
@@ -1,8 +1,3 @@
-/*
-	This component (Detail Modal) serves as a card that displays information related to a selected movie or show, including information
-	such as a show overview, title, release data, genre, and more. The functions for the component are declared and the entire card/modal is
-	returned.
-*/
 import './detailModal.scss'
 import { useRef } from 'react';
 import { Link } from "react-router-dom";
@@ -18,40 +13,61 @@ import { FaMinus, FaPlay, FaPlus } from "react-icons/fa";
 import { addToFavourites, removeFromFavourites } from "../../redux/favourites/favourites.actions";
 import useOutsideClick from "../../hooks/useOutsideClick";
 
+/*
+	A Modal component that displays details of the selected movie or series.
+
+	This component (Detail Modal) serves as a card that displays information related to a selected movie or show, including information
+	such as a show overview, title, release data, genre, and more. It also maintains a play button which would play the series or video if 
+	there was one, but rather it plays the FakeFlix logo instead as this is a demo and not an actual streaming service like Netflix.
+
+	Returns: A JSX.element that serves as the detail modal component within the page.
+*/
+
 const DetailModal = () => {
-
-	// Section for redux functions/setup.
+	/* 
+		Section for redux functions such as dispatch and selectors for factors such as if the modal is open or not,
+		what content should be in the modal, and what function will be used to close or hide the modal.
+	*/
 	const dispatch = useDispatch();
-	const modalClosed = useSelector(selectModalState);	// Determines if the modal is open or not.
-	const modalContent = useSelector(selectModalContent);	// Contains the content within the modal (genres/overview/dates/etc).
+	const modalClosed = useSelector(selectModalState);
+	const modalContent = useSelector(selectModalContent);
 
-	const handleModalClose = () => dispatch(hideModalDetail());	// Function referencing another file's function which closes/hides the modal.
+	const handleModalClose = () => dispatch(hideModalDetail());	
 
+	/*
+		Section for distributing/adjusting/handling modal details and content. This includes organizing all the data 
+		from modalContent into easily distinguishable and accessable components, organizing the list of genres, defining if the content is
+		for adults, and formatting the airing date.
+	*/
 	const { overview, fallbackTitle, backdrop_path, release_date, first_air_date, vote_average, original_language, adult, genresConverted, isFavourite } = modalContent;
-	const joinedGenres = genresConverted ? genresConverted.join(', ') : "Not available";
+	const joinedGenres = genresConverted ? genresConverted.join(', ') : "Not available";	
 	const maturityRating = adult === undefined ? "Not available" : adult ? "Suitable for adults only" : "Suitable for all ages";
 	const reducedDate = release_date ? dateToYearOnly(release_date) : first_air_date ? dateToYearOnly(first_air_date) : "Not Available";
 	const modalRef = useRef();
 
 	// Section which primarily refers to major functions that occur on a button click or action related to the detail modal.
 
-	const handleAdd = (event) => {	// HandleAdd is called when the user selects to favorite a show or movie. 
+	// HandleAdd is called when the user selects to favorite a show or movie. 
+	const handleAdd = (event) => {	
 		event.stopPropagation();
 		dispatch(addToFavourites({ ...modalContent, isFavourite }));	// Calls a function from favourites.actions which adds the selected item to a favorites list.
 	}
 
-	const handleRemove = (event) => {	// HandleRemove is called when the user selects to unfavorite a show or movie. 
+	// HandleRemove is called when the user selects to unfavorite a show or movie. 
+	const handleRemove = (event) => {	
 		event.stopPropagation();
 		dispatch(removeFromFavourites({ ...modalContent, isFavourite }));	// Calls a function from favourites.actions which removes the selected item from the favorites list.
 		if (!modalClosed) handleModalClose();	// Closes the modal afterwards (if it was not already closed.)
 	}
 
+	// HandlePlayAnimation closes the modal and plays the FakeFlix animation upon pressing the play button (as there are no movies/series to actually play).
 	const handlePlayAnimation = event => {
 		event.stopPropagation();
 		handleModalClose();
 	};
 	
-	useOutsideClick(modalRef, () => {	// Uses a custom hook within the program that closes the modal when clicked outside the bounds.	
+	// Uses a custom hook within the program that closes the modal when clicked outside the bounds.	
+	useOutsideClick(modalRef, () => {	
 		if (!modalClosed) handleModalClose();
 	});
 
