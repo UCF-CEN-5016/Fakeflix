196afb997463cd3d3cb4be3f41ed6cc953cd62c7
Evan <pilling2003@gmail.com>
2024-09-17 15:15:48 -0400

    Documenting PlayAnimation.jsx

--- src/components/PlayAnimation/PlayAnimation.jsx
+++ src/components/PlayAnimation/PlayAnimation.jsx
@@ -3,15 +3,23 @@ import { useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { TADUM_SOUND_URL } from "../../requests";
 
+/** The purpsoe of this file is to trigger the Netflix *tadum* sound and animation and then reroute the user to the browing page. */
+
+/** This is the main function that triggers everything in the file. */
 const PlayAnimation = () => {
 
 	let history = useHistory();
 	const soundRef = useRef(null);
+
+	/** This function's purpose is to play the tadum sound that soundRef references. */
 	const handleTadum = () => {
+
+		/** The purpose of the below lines is to make sure the *tadum sound plays from the beginning* */
 		soundRef.current.currentTime = 0;
 		soundRef.current.play();
 	}
 
+	/** An anonymous function is passed to the useEffect hook. This triggers the *tadum* sound and reroutes the user after 4.2 seconds. */
 	useEffect(() => {
 		handleTadum();
 		setTimeout(() => {
@@ -19,6 +27,7 @@ const PlayAnimation = () => {
 		}, 4200)
 	}, [history])
 
+	/** This portion of the code renders the .jsx FAKEFLIX animation. */
 	return (
 		<div className='PlayAnimation__wrp'>
 			<audio ref={soundRef} src={TADUM_SOUND_URL} />
