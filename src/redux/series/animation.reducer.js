a88d340e7e3684a1a127813bbfcc41a4e7364c58
Salman H <137637055+shamada05@users.noreply.github.com>
2024-09-20 20:38:29 -0400

    Update animation.reducer.js
    Comments are added to the code to explain the various lines

--- src/redux/series/animation.reducer.js
+++ src/redux/series/animation.reducer.js
@@ -1,18 +1,46 @@
+/*
+    This file creates a reducer function named 'animationSeriesReducer' that changes the state of the data for the animation series.
+    It imports 'seriesActionTypes' from './series.types', creates a construct for the initial state, and then cycles through the different cases to fetch data.
+*/
+
 import { seriesActionTypes } from './series.types';
 
+/* 
+This construct creates a variable holding the initial state of the reducer.
+    loading: A boolean that determines whether data is being gathered
+    error: A string that stores error messages if needed
+    data: An array that holds the gathered data
+*/
 const initialState = {
     loading: false,
     error: '',
     data: []
 }
 
+/*
+This is a reducer function that takes:
+    - A variable 'state' that defaults to 'initialState' 
+    - An object with a 'type' and 'payload' property
+It then uses these to determine the new state based on the type and payload
+*/
 const animationSeriesReducer = (state = initialState, {type, payload}) => {
+    // This determines what happened based on the received 'type' variable from the previous line
     switch (type) {
+            /*
+            When the type signifies that a fetch is being made, this case will:
+                - Set the loading variable to true
+            */
         case seriesActionTypes.FETCH_ANIMATION_SERIES_REQUEST:
             return {
                 ...state,
                 loading: true
             }
+            /* 
+            If a fetch is successful, this case will:
+                - Update the data variable with the obtained data
+                - Set the loading variable to false
+                - Get rid of error messages if there are any
+            */
         case seriesActionTypes.FETCH_ANIMATION_SERIES_SUCCESS:
             return {
                 ...state,
@@ -20,6 +48,12 @@ const animationSeriesReducer = (state = initialState, {type, payload}) => {
                 loading: false,
                 error: ''
             }
+            /*
+            If there are more than one animation series, this case will:
+                - Add the newly obtained data to the existing data
+                - Set the loading variable to false
+                - Get rid of error messages if there are any
+            */
         case seriesActionTypes.LOAD_MORE_ANIMATION_SERIES_SUCCESS:
             return {
                 ...state,
@@ -27,6 +61,12 @@ const animationSeriesReducer = (state = initialState, {type, payload}) => {
                 loading: false,
                 error: ''
             }
+            /*
+            If there is some failure when fetching the data, this case will:
+                - Reset any data in the variable
+                - Set the loading variable to false
+                - Add an error message with the obtained payload variable
+            */
         case seriesActionTypes.FETCH_ANIMATION_SERIES_FAILURE:
             return {
                 ...state,
@@ -34,9 +74,11 @@ const animationSeriesReducer = (state = initialState, {type, payload}) => {
                 loading: false,
                 error: payload
             }
+        // This default case returns the current state without modifying it
         default:
             return state;
     }
 }
 
+// This line exports the function above as the default export
 export default animationSeriesReducer;
